c4aaee8ef87adaff10efae3b1c0085f74e3f6a26
JiayuanWX <jwang005@ikasle.ehu.eus>
2024-03-19 12:34:26 +0100

    login, register and favourite rides UI design

---
+++ src/main/java/eus/ehu/sharetrip/uicontrollers/SignUpController.java
@@ -0,0 +1,19 @@
+package eus.ehu.sharetrip.uicontrollers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class SignUpController {
+
+    @FXML
+    private TextField login;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private ComboBox<String> roles;
+
+}
