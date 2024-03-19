506b70cd0f549a81a58f626c401567e4e7ce3781
inigovilaa <ivila006@ikasle.ehu.eus>
2024-03-19 13:15:30 +0100

    New classes (traveler, user...)

---
+++ src/main/java/eus/ehu/sharetrip/domain/Traveler.java
@@ -0,0 +1,11 @@
+package eus.ehu.sharetrip.domain;
+
+
+public class Traveler extends User {
+    public Traveler(String userName, String password, String email) {
+        super(userName, password, email);
+    }
+
+    public Traveler() {
+    }
+}
---
+++ src/main/java/eus/ehu/sharetrip/uicontrollers/LoginController.java
@@ -0,0 +1,70 @@
+package eus.ehu.sharetrip.uicontrollers;
+
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+/*
+public class LoginController implements Controller {
+
+    //private final BlFacade bl;
+
+    @FXML
+    private TextField login;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private ComboBox<String> roles;
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+    //private MainGUI mainGUI;
+
+    @FXML
+    void onClick(ActionEvent event) {
+
+        if (login.getText() == null
+                || password.getText() == null ||
+                roles.getValue() == null)
+            return;
+
+        //try {
+          //  bl.login(login.getText(), password.getText());
+            //mainGUI.showGrading();
+        //} //catch (UnknownUser unknownUser) {
+            //System.out.println("Unknown user");
+        //} catch (IOException e) {
+         //   throw new RuntimeException(e);
+        //}
+
+    }
+
+    //@FXML
+    //void initialize() {
+      ///  ObservableList<String> options = FXCollections.observableArrayList("Teacher", "Student");
+        //roles.setItems(options);
+   // }
+
+   // @Override
+   // public void setMainApp(MainGUI mainGUI) {
+     //   this.mainGUI = mainGUI;
+    //}
+
+    //public LoginController(BlFacade bl) {
+      //  this.bl = bl;
+//    }
+}
+*/
