f942f3d9f6cccef54871c39529d2d2f9f4516ed8
Wolfxyz16 <yerayliloaiza@gmail.com>
2024-03-19 13:05:12 +0100

    little refactor

--- src/main/java/eus/ehu/ridesfx/uicontrollers/Controller.java
+++
@@ -1,7 +0,0 @@
-package eus.ehu.ridesfx.uicontrollers;
-
-import eus.ehu.ridesfx.ui.MainGUI;
-
-public interface Controller {
-  void setMainApp(MainGUI mainGUI);
-}
---
+++ src/main/java/eus/ehu/sharetrip/uicontrollers/Controller.java
@@ -0,0 +1,7 @@
+package eus.ehu.sharetrip.uicontrollers;
+
+import eus.ehu.sharetrip.ui.MainGUI;
+
+public interface Controller {
+  void setMainApp(MainGUI mainGUI);
+}
