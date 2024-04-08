05f9edfb3bb51c84e69a5e5cd821cbd4b9eb99ef
JiayuanWX <jwang005@ikasle.ehu.eus>
2024-04-08 23:37:47 +0200

    For merging

--- src/main/java/eus/ehu/sharetrip/ui/MainGUI.java
+++ src/main/java/eus/ehu/sharetrip/ui/MainGUI.java
@@ -45,7 +45,7 @@ public class MainGUI {
         });
     }
 
-    private Window mainWin, createRideWin, queryRidesWin, loginWin, registerWin, favoriteOverviewWin, chatOverviewWin, alertOverviewWin, createCityWin, alertOverviewWin;
+    private Window mainWin, createRideWin, queryRidesWin, loginWin, registerWin, favoriteOverviewWin, chatOverviewWin, alertOverviewWin, createCityWin, messagesOverviewWin, sendMessageWin, viewMessagesWin;
 
     class Window {
         Controller c;
